f0909b45a5adb7d670262c5d0153f176b4754657
Peter Menocal <peter@geekypixel.com>
2021-03-25 11:42:07 -0700

    update

--- pages/deliveryorder.vue
+++ pages/deliveryorder.vue
@@ -4,7 +4,13 @@
       src="/order-delivery.html"
       height="100%"
       width="100%"
-      style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden;"
+      style="position:fixed; top:90px; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden;"
     />
   </div>
 </template>
+<style>
+body {
+  margin: 0 !important;
+  padding: 0 !important;
+}
+</style>
--- pages/storeorder.vue
+++ pages/storeorder.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    <script src="https://topnotch.wm.store​/static/js/embed.js"></script>
+    <iframe
+      src="/order-delivery.html"
+      height="100%"
+      width="100%"
+      style="position:fixed; top:90px; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden;"
+    />
   </div>
 </template>
+<style>
+body {
+  margin: 0 !important;
+  padding: 0 !important;
+}
+</style>
