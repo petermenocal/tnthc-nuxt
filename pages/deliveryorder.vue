f4a52b9f2788dba4cfaf2c6c6bd76e9b6dca56fe
Peter Menocal <peter@geekypixel.com>
2021-03-25 11:05:42 -0700

    test

--- pages/deliveryorder.vue
+++ pages/deliveryorder.vue
@@ -3,3 +3,13 @@
     <script src="https://topnotch.wm.store​/static/js/embed.js"></script>
   </div>
 </template>
+<script>
+// Add script tag to head
+if (process.browser) {
+  let script = document.createElement("script");
+  script.async = true;
+  script.id = "weedmaps";
+  script.src = "https://topnotch.wm.store​/static/js/embed.js";
+  document.body.appendChild(script);
+}
+</script>
