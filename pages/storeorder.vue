260f4d431edad63245ac1d8bce504f11d8b71ba3
Peter Menocal <peter@geekypixel.com>
2021-03-25 11:45:57 -0700

    update

--- pages/storeorder.vue
+++ pages/storeorder.vue
@@ -4,6 +4,11 @@
       src="/order-delivery.html"
       height="100%"
       width="100%"
+      marginwidth="0"
+      marginheight="0"
+      hspace="0"
+      vspace="0"
+      frameborder="0"
       style="position:fixed; top:90px; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden;"
     />
   </div>
