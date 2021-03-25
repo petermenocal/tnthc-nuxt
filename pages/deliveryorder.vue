7d2f57eb6c0a9fd4dec4e4bdb9fd7891d3a5cb5c
Peter Menocal <peter@geekypixel.com>
2021-03-25 11:12:10 -0700

    update

--- pages/deliveryorder.vue
+++ pages/deliveryorder.vue
@@ -5,11 +5,15 @@
 </template>
 <script>
 // Add script tag to head
-if (process.browser) {
-  let script = document.createElement("script");
-  script.async = true;
-  script.id = "weedmaps";
-  script.src = "https://topnotch.wm.store​/static/js/embed.js";
-  document.body.appendChild(script);
-}
+export default {
+  mounted() {
+    if (process.browser) {
+      let script = document.createElement("script");
+      script.async = true;
+      script.id = "weedmaps";
+      script.src = "https://topnotch.wm.store​/static/js/embed.js";
+      document.body.appendChild(script);
+    }
+  }
+};
 </script>
