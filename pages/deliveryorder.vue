0a11354b92a722115ee2beda1480b0a08b93d42c
Peter Menocal <peter@geekypixel.com>
2021-03-25 11:34:42 -0700

    update

--- pages/deliveryorder.vue
+++ pages/deliveryorder.vue
@@ -1,10 +1,10 @@
 <template>
-  <div style="position:relative">
+  <div style="position: relative !important;">
     <iframe
       src="/order-delivery.html"
       height="100%"
       width="100%"
-      style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"
+      style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden;"
     />
   </div>
 </template>
