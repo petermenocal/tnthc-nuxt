4e4db8006fbefab29202b3dfbf5b37c550b577ef
Peter Menocal <peter@geekypixel.com>
2021-03-26 12:51:07 -0700

    update

---
+++ pages/diagnostic.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="wrapper-diag">
+      <p>Rendered on {{ renderedOn }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  asyncData() {
+    return {
+      content: "Seconds: " + new Date().getSeconds(),
+      renderedOn: process.client ? "client" : "server"
+    };
+  }
+};
+</script>
+
+<style>
+.wrapper-diag {
+  padding: 40px;
+  width: 90%;
+  margin: 0 auto;
+}
+</style>
