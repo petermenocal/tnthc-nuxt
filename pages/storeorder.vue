c3647a1279fb8b184309e5e533dec91928ef3358
Peter Menocal <peter@geekypixel.com>
2021-04-05 08:37:14 -0700

    add margin to weedmaps

--- pages/storeorder.vue
+++ pages/storeorder.vue
@@ -15,7 +15,7 @@
       hspace="0"
       vspace="0"
       frameborder="0"
-      style="position:fixed; top:130px; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden;"
+      style="position:fixed; top:130px; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; margin-bottom: 100px !important; padding:0; overflow:hidden;"
     />
   </div>
 </template>
